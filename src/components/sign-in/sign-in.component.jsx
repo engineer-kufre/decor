5f2e72a7cbc73efdc0ee8cb9f2804a4b8cc4a4c5
Kufreabasi <engineer_kufre@yahoo.com>
2021-01-06 23:46:37 +0100

    finish sign in

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import CustomButton from '../custom-button/custom-button.component';
 import FormInput from '../form-input/form-input.component';
 import { Link } from 'react-router-dom'
-import {signInWithGoogle} from '../../firebase/firebase.utils'
+import {auth, signInWithGoogle} from '../../firebase/firebase.utils'
 import './sign-in.styles.scss'
 
 export default class SignIn extends Component {
@@ -15,10 +15,17 @@ export default class SignIn extends Component {
     }
   }
 
-  handleSubmit = e => {
+  handleSubmit = async e => {
     e.preventDefault();
 
-    this.setState({email: '', password: ''})
+    const {email, password} = this.state;
+
+    try{
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({email: '', password: ''});
+    } catch (error){
+      console.log(error);
+    }
   }
 
   handleChange = e => {
